7aa6d006cf792e4e55831c34a13e903248d99df6
Quamin Hua <quaminhua56@gmail.com>
2023-03-11 00:42:13 -0800

    started work on DistantMatch + comment on iMatch

--- src/main/java/com/example/final_tmge/src/DistantMatch.java
+++ src/main/java/com/example/final_tmge/src/DistantMatch.java
@@ -5,6 +5,14 @@ import java.util.ArrayList;
 
 class DistantMatch implements iMatch {
     public List<List<Coordinates>> findMatches(GamePiece[][] board) {
-        return new ArrayList<List<Coordinates>>();
+        int height = board.length;
+        int width = board[0].length;
+        List<List<Coordinates>> matchesList = new ArrayList<List<Coordinates>>();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                
+            }
+        }
+        return matchesList;
     }
 }
--- src/main/java/com/example/final_tmge/src/iMatch.java
+++ src/main/java/com/example/final_tmge/src/iMatch.java
@@ -3,5 +3,8 @@ package com.example.final_tmge.src;
 import java.util.List;
 
 public interface iMatch {
+    // Takes a game board (a 2D array of GamePiece class objects)
+    // as a parameter and returns a List of all respective matches,
+    // with each match being in the form of a List of Coordinates
     public List<List<Coordinates>> findMatches(GamePiece[][] board);
 }
