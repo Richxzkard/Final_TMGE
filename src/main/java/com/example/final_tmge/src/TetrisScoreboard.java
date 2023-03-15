edfb02ce9b5b327c74025fc1d01b81325c338257
yav4-e <yavin.evenich@gmail.com>
2023-03-14 18:21:43 -0700

    added timer and timertask inherited class in TetrisScoreboard.java

--- src/main/java/com/example/final_tmge/src/TetrisScoreboard.java
+++ src/main/java/com/example/final_tmge/src/TetrisScoreboard.java
@@ -1,4 +1,45 @@
 package com.example.final_tmge.src;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class TetrisScoreboard {
+
+    class CheckMatchTask extends TimerTask{
+
+        @Override
+        public void run(){
+
+        }
+    }
+
+
+    private int player1Score;
+    private int player2Score;
+    int players;
+
+    Timer timer;
+
+    boolean gameIsLive;
+
+    TetrisScoreboard(int players){
+
+        this.players = players;
+        timer = new Timer();
+
+
+        if(players == 1){
+            player1Score = 0;
+        }
+        else{
+            player1Score = 0;
+            player2Score = 0;
+        }
+
+    }
+
+
+
+
+
 }
