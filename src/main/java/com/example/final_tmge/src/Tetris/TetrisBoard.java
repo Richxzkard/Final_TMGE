1ce909797874ca10b9d509f954eb9dc0d5976453
yav4-e <yavin.evenich@gmail.com>
2023-03-16 17:22:22 -0700

    Added a GamePiece[][] board and a getBoardPieces() function in TetrisBoard.java

--- src/main/java/com/example/final_tmge/src/Tetris/TetrisBoard.java
+++ src/main/java/com/example/final_tmge/src/Tetris/TetrisBoard.java
@@ -1,11 +1,20 @@
 package com.example.final_tmge.src.Tetris;
 
 import com.example.final_tmge.src.Board;
+import com.example.final_tmge.src.GamePiece;
 
 public class TetrisBoard extends Board {
 
+    GamePiece[][] board;
+
     public TetrisBoard(int height, int width) {
         super(height, width);
+        board = new GamePiece[height][width];
     }
+
+    public GamePiece[][] getBoardPieces(){
+        return board;
+    }
+
 
 }
--- src/main/java/com/example/final_tmge/src/TetrisScoreboard.java
+++ src/main/java/com/example/final_tmge/src/TetrisScoreboard.java
@@ -1,32 +1,36 @@
 package com.example.final_tmge.src;
 
+import com.example.final_tmge.src.Tetris.TetrisBoard;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class TetrisScoreboard {
 
     class CheckMatchTask extends TimerTask{
-
         @Override
         public void run(){
-
+            match.findMatches(board.getBoardPieces());
         }
     }
 
-
     private int player1Score;
     private int player2Score;
+    TetrisBoard board;
+    HorizontalMatch match;
     int players;
-
     Timer timer;
-
     boolean gameIsLive;
+    CheckMatchTask task;
 
-    TetrisScoreboard(int players){
+    TetrisScoreboard(int players, TetrisBoard board){
 
         this.players = players;
-        timer = new Timer();
+        this.board = board;
 
+        timer = new Timer();
+        match = new HorizontalMatch();
+        task = new CheckMatchTask();
 
         if(players == 1){
             player1Score = 0;
@@ -35,6 +39,8 @@ public class TetrisScoreboard {
             player1Score = 0;
             player2Score = 0;
         }
+
+        timer.schedule(task, 1000, 1000);
 
     }
 
